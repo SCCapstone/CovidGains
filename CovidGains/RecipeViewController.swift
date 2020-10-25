51f0652bb8c0cc9a947d4002223ce94cda493b6c
Nishat Tabassum <nishattabassum24@yahoo.com>
2020-10-25 14:15:45 -0400

    Story Board

--- CovidGains/RecipeViewController.swift
+++ CovidGains/RecipeViewController.swift
@@ -11,7 +11,7 @@ class RecipeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//This is nishat comment or conflict
+//This is nishat's conflict
     }
     
 
