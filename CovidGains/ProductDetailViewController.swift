d2a05a93b5f36205c644c9d8854da73ed5db709b
Robert Carey <rtcarey@email.sc.edu>
2020-10-25 13:02:50 -0400

    File Additions

---
+++ CovidGains/ProductDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProductDetailViewController.swift
+//  Hello World 2
+//
+//  Created by CAREY, ROBERT T on 10/9/20.
+//  Copyright © 2020 CAREY, ROBERT T. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    var pName: String = ""
+    @IBOutlet weak var productName: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "doneSegue" {
+            pName = productName.text!
+        }
+    }
+    //
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CovidGains/ProductListViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ProductListViewController.swift
+//  Hello World 2
+//
+//  Created by CAREY, ROBERT T on 10/9/20.
+//  Copyright © 2020 CAREY, ROBERT T. All rights reserved.
+//
+
+import UIKit
+
+class ProductListViewController: UITableViewController {
+    
+    
+    var products = [String]()
+    var newProduct: String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        products = ["Apple","Bannana","Strawberry"]
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return products.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = products[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    @IBAction func cancel(segue:UIStoryboardSegue) {
+      
+    }
+
+    @IBAction func done(segue:UIStoryboardSegue) {
+         let productDetailVC = segue.source as! ProductDetailViewController
+         newProduct = productDetailVC.pName
+            
+         products.append(newProduct)
+         tableView.reloadData()
+        
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
