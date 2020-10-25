a311630cbc711b49f19a287f7ecfc93dc000d8b0
patelmaitri <maitri.patel293@gmail.com>
2020-10-25 13:55:53 -0400

    RecipeViewController

---
+++ CovidGains/RecipeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RecipeViewController.swift
+//  CovidGains
+//
+//  Created by Maitri Patel on 10/25/20.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+// Conflit for Maitri Patel
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
