ff6d3cede7db92bf583f81a2254358a1341059fb
patelmaitri <maitri.patel293@gmail.com>
2020-10-25 19:01:22 -0400

    message

--- CovidGains/ViewController.swift
+++ CovidGains/ViewController.swift
@@ -8,18 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-<<<<<<< HEAD
-//Maitri 10/25/2020
-=======
-//Praful on 10/25/20
->>>>>>> f4e24180e596672e4b59f35a098311d68d1841a8
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func HomeButton(_ sender: Any) {
-    }
-    
+
 }
 
