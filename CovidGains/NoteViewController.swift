989fc994e817fe46a6c21e1570be2c68a0535706
patelmaitri <maitri.patel293@gmail.com>
2020-11-18 22:38:33 -0500

    notes tab

---
+++ CovidGains/NoteViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class NoteViewController: UIViewController {
+    @IBOutlet weak var titelLabel: UILabel!
+    
+    @IBOutlet weak var noteLabel: UITextView!
+    public var noteTitle: String = ""
+    public var note: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titelLabel.text = noteTitle
+        noteLabel.text = note
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
