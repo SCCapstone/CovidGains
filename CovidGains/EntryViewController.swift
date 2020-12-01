7f614d146099c32473b523be532bb8ee0a6bfdeb
patelmaitri <maitri.patel293@gmail.com>
2020-12-01 11:57:10 -0500

    save notes

--- CovidGains/EntryViewController.swift
+++ CovidGains/EntryViewController.swift
@@ -20,42 +20,26 @@ class EntryViewController: UIViewController {
         titleField.becomeFirstResponder() //showing keyboard
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveButton))
 
-//        let appDelegate = UIApplication.shared.delegate as? AppDelegate
-//
-//        let managedContext = appDelegate.managedObjectContext
-//
-//        let entiry = NSEntityDescription.entity(forEntityName: "EntryEntity", in: managedContext)
-//
-//        let item = NSManagedObject(entity: entiry!, insertInto: managedContext)
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-
-        //let appDelegate = UIApplication.shared.delegate as? AppDelegate
-
-//        let managedContext = appDelegate.managedObjectContext
-//
-//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "EntryEntity")
-    }
-
 
     @objc func saveButton(){
         //only if text in it
         if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty{
             completion?(text, noteField.text)
         }
-
-        let completeUrl = URL(fileURLWithPath: "./docs/"+textField.text+".txt")
-        let completeUrlString = "./docs/"+textField.text+".txt"
-        if (fileManager.fileExist(atPath:completeUrl)) {
-          let tempUrl = URL(fileURLWithPath: "./docs/temp.txt")
-          let tempUrlString = "./docs/temp.txt"
-          fileManager.createFile(atPath: tempUrlString, contents: noteField.text)
-          fileManager.replaceItem(at: completeUrl, withItemAt: tempUrl)
-          fileManager.removeItem(at:tempUrl)
-        } else{
-          fileManager.createFile(atPath: completeUrlString, contents: noteField.text)
-        }
+        //vc.noteTitle = model.title
+        //vc.note = model.note
+//        let completeUrl = URL(fileURLWithPath: "./docs/", models.title,".txt")
+//        let completeUrlString = "./docs/"+textField.text+".txt"
+//        if (fileManager.fileExist(atPath:completeUrl)) {
+//          let tempUrl = URL(fileURLWithPath: "./docs/temp.txt")
+//          let tempUrlString = "./docs/temp.txt"
+//          fileManager.createFile(atPath: tempUrlString, contents: noteField.text)
+//          fileManager.replaceItem(at: completeUrl, withItemAt: tempUrl)
+//          fileManager.removeItem(at:tempUrl)
+//        } else{
+//          fileManager.createFile(atPath: completeUrlString, contents: noteField.text)
+//        }
 
     }
 
