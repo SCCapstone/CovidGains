a335cf7e27aebd945df0ea7a99bb47c795291c16
patelmaitri <maitri.patel293@gmail.com>
2020-12-01 12:30:41 -0500

    deleteing the product from expiry

--- CovidGains/ProductListViewController.swift
+++ CovidGains/ProductListViewController.swift
@@ -66,6 +66,7 @@ class ProductListViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
