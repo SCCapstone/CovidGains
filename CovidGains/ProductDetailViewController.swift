238d0aa5e7eba9493ba227f865d7e0ad848e97c1
Praful Chunchu <prafulchunchu@gmail.com>
2020-11-30 19:35:16 -0500

    update

--- CovidGains/ProductDetailViewController.swift
+++ CovidGains/ProductDetailViewController.swift
@@ -23,16 +23,8 @@ class ProductDetailViewController: UIViewController {
             pName = productName.text!
         }
     }
-    //
     
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
