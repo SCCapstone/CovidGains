b254a7c974943ef2f5fb1dfea1f4d9e70158dfbb
patelmaitri <maitri.patel293@gmail.com>
2020-12-02 18:52:21 -0500

    recipe other tab

---
+++ CovidGains/Recipe/DetailedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailedViewController.swift
+//  CovidGains
+//
+//  Created by Maitri Patel on 12/2/20.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBOutlet weak var ingredients: UITextView!
+    
+    @IBOutlet weak var steps: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
