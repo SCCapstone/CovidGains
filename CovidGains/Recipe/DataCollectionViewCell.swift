6ea6ef8f3abe30e1bab058f626cec1aea344ad76
Robert Carey <rtcarey@email.sc.edu>
2020-12-02 14:24:32 -0500

    recipe collection view

---
+++ CovidGains/Recipe/DataCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DataCollectionViewCell.swift
+//  CovidGains
+//
+//  Created by CAREY, ROBERT T on 12/2/20.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
+    
+    
+}
--- CovidGains/Recipe/RecipeViewController.swift
+++ CovidGains/Recipe/RecipeViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class RecipeViewController: UIViewController {
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var imageArr = ["Broccoli Pesto Pasta", "Mashed Potatoes", "Lemon Chicken", "Pasta with Chickpea Sauce", "Mac and Cheese", "Smoky Potatoes", "Greek Spaghetti", "Nutty Rice", "Pizza", "Alfraedo Chicken"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         /*
@@ -21,8 +27,11 @@ class RecipeViewController: UIViewController {
         dishes["sandwich"] = {"lettuce", "tomato", "ham", "bacon"]
         dishes["macaroni"] = {"pasta", "milk", "butter", "cheese"}
         */
+        
     }
     
+
+
 
     /*
     // MARK: - Navigation
@@ -34,4 +43,20 @@ class RecipeViewController: UIViewController {
     }
     */
 
+}
+
+
+extension RecipeViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArr.count
+    }
+     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath : IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DataCollectionViewCell
+        cell?.img.image = UIImage(named: imageArr[indexPath.row])
+        cell?.lbl.text = imageArr[indexPath.row]
+        
+        return cell!
+    }
 }
