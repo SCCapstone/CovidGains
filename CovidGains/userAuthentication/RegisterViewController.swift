bf6a394a40763df81044a25711233aaeb746e7c7
Praful Chunchu <prafulchunchu@gmail.com>
2020-12-03 22:50:32 -0500

    user activation

---
+++ CovidGains/userAuthentication/RegisterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterViewController.swift
+//  CovidGains
+//
+//  Created by Praful Chunchu on 12/3/20.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController{
+    
+    
+}
