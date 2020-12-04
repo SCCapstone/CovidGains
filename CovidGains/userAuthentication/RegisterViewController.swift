86cdecaa97b95b9aa0639f245e69dd741fe546de
Praful Chunchu <prafulchunchu@gmail.com>
2020-12-03 23:56:43 -0500

    user registration

--- CovidGains/userAuthentication/RegisterViewController.swift
+++ CovidGains/userAuthentication/RegisterViewController.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController{
     
+    @IBOutlet weak var emailTextfield: UITextField!
     
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) {  authResult, error in
+            
+                if let e = error
+                {
+                    print(e)
+                }
+                else{
+                    self.performSegue(withIdentifier: "RegisterToGrosh", sender: self)
+                    
+                }
+            
+      
+            }
+        }
+    }
 }
