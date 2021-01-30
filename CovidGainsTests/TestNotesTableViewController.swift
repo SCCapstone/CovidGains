b31acae1605d16156f288fcc303a5f414f3f28ae
Praful Chunchu <prafulchunchu@Prafuls-MacBook-Pro.local>
2021-01-30 16:30:02 -0500

    updating number of tests

--- CovidGainsTests/TestNotesTableViewController.swift
+++ CovidGainsTests/TestNotesTableViewController.swift
@@ -11,11 +11,27 @@ class TestNotesTableViewController: XCTestCase {
     var notes = [[String:String]]()
    
     func testNotesAdded() throws {
+        
+        //Unit test 1
         let note = ["Title": "", "Body": ""]
         notes.insert(note, at: 0)
         print(notes[0])
         XCTAssertEqual(notes[0], ["Title": "", "Body": ""])
+        
+        // Unit test 2
+        let note2 = ["Test": "", "Value": ""]
+        notes.insert(note2, at: 0)
+        print(notes[0])
+        XCTAssertEqual(notes[0], ["Test": "", "Value": ""])
+        
+        //Unit test 3
+        let note3 = ["Ingredient": "", "Pepper": ""]
+        notes.insert(note3, at: 0)
+        print(notes[0])
+        XCTAssertEqual(notes[0], ["Ingredient": "", "Pepper": ""])
     }
+    
+    
     
 
 
