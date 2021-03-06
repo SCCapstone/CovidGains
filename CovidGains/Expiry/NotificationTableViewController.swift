e9ebb46f1784959f2c064894c8ac91b603e819b1
Maitri Patel <maitri.patel293@gmail.com>
2021-03-06 13:54:16 -0500

    show the date on the expiry

--- CovidGains/Expiry/NotificationTableViewController.swift
+++ CovidGains/Expiry/NotificationTableViewController.swift
@@ -130,12 +130,12 @@ class NotificationTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = productData[indexPath.row].productName
         
-//        let date = productData[indexPath.row].date
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "MM, dd, YYYY at hh:mm a"
-//        cell.detailTextLabel?.text = formatter.string(from: date)
+        let date = productData[indexPath.row].date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM, dd, YYYY at hh:mm a"
+        cell.detailTextLabel?.text = formatter.string(from: date)
         
-        cell.detailTextLabel?.text = productData[indexPath.row].productDetail
+        //cell.detailTextLabel?.text = formatter.string(from: date)
 
         return cell
     }
--- CovidGainsUITests/CovidGainsUITests.swift
+++ CovidGainsUITests/CovidGainsUITests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Firebase
 
 var app: XCUIApplication!
 
