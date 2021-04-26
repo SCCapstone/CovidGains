4db556ca640d883a803a49efddfe7e34dbbb09e9
Maitri Patel <maitri.patel293@gmail.com>
2021-04-26 16:49:30 -0400

    test file

--- CovidGainsTests/TestNotificationTableViewController.swift
+++ CovidGainsTests/TestNotificationTableViewController.swift
@@ -2,35 +2,16 @@
 //  TestNotificationTableViewController.swift
 //  CovidGainsTests
 //
-//  Created by CAREY, ROBERT T on 1/30/21.
+//  Created by Maitri Patel on 4/26/21.
 //
 
+import Foundation
 import XCTest
-import CovidGains
-import Firebase
-class TestNotificationTableViewController: XCTestCase {
-   
-    
-    func testDatabaseAdd() throws {
-        let db = Firestore.firestore()
-        db.collection("Test").document("doc").setData(["Quantity":"1"])
-        db.collection("Test").getDocuments { (querySnapshot, error) in
-            if let e = error {
-                print("There is issue retrieving data. \(e)")
-            } else {
-                if let snapshotDocuments = querySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let teststr = doc.documentID
-                        XCTAssertEqual("doc", "fail")
-                        
-                    }
-                }
-                
-            }
-        }
-    
+@testable import CovidGains
+
+class TestNotificationTableViewController: XCTestCase{
+    func testFunc(){
+        let newVC = NotificationTableViewController()
+        
     }
-
-   
-
 }
