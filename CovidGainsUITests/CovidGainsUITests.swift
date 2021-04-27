99ad7680f329084a6181c3a78333093164f138f3
Maitri Patel <maitri.patel293@gmail.com>
2021-04-26 20:51:06 -0400

    UI test udpated

--- CovidGainsUITests/CovidGainsUITests.swift
+++ CovidGainsUITests/CovidGainsUITests.swift
@@ -4,78 +4,131 @@
     //
     //  Created by KIM, JEEJUN on 1/20/21.
     //
-
+    
     import XCTest
     import CovidGains
     import Firebase
-
+    
     var app: XCUIApplication!
-
+    
     class CovidGainsUITests: XCTestCase {
-
+        
         override func setUpWithError() throws {
             // Put setup code here. This method is called before the invocation of each test method in the class.
             app = XCUIApplication()
             app.launch()
-
+            
             // In UI tests it is usually best to stop immediately when a failure occurs.
             continueAfterFailure = false
-
+            
             // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         }
-
+        
         override func tearDownWithError() throws {
             // Put teardown code here. This method is called after the invocation of each test method in the class.
         }
-
-
-    
-    func testLogin() throws {
+        
+    func testRegister() throws {
+        
         let app = XCUIApplication()
-        app.launch()
-        app.buttons["Login"].tap()
-        //app.buttons["Back"].tap()
-        //app.buttons["Register"].tap()
-        let testEmail = app.otherElements.textFields["Username"]
-        let testPass = app.otherElements.secureTextFields["Password"]
-        testEmail.tap()
-        testEmail.typeText("1@2.com")
-        testPass.tap()
-        testPass.typeText("123456")
-        app.buttons["Login"].tap()
+        let registerButton = app.buttons["Register"]
+        registerButton.tap()
+        app.textFields["Email"].tap()
+        
+        let moreKey = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        moreKey.tap()
+
+        
+        let key = app/*@START_MENU_TOKEN@*/.keys["1"]/*[[".keyboards.keys[\"1\"]",".keys[\"1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key.tap()
+
+        
+        let key2 = app/*@START_MENU_TOKEN@*/.keys["@"]/*[[".keyboards.keys[\"@\"]",".keys[\"@\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key2.tap()
+
+        
+        let key3 = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key3.tap()
+
+        
+        let key4 = app/*@START_MENU_TOKEN@*/.keys["."]/*[[".keyboards.keys[\".\"]",".keys[\".\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key4.tap()
+        
+        let moreKey2 = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"letters\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        moreKey2.tap()
+
+        
+        let cKey = app/*@START_MENU_TOKEN@*/.keys["c"]/*[[".keyboards.keys[\"c\"]",".keys[\"c\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        cKey.tap()
+  
+        
+        let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        oKey.tap()
+
+        
+        let mKey = app/*@START_MENU_TOKEN@*/.keys["m"]/*[[".keyboards.keys[\"m\"]",".keys[\"m\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        mKey.tap()
+
+        app.secureTextFields["Password"].tap()
+        moreKey.tap()
+
+        
+        let key5 = app/*@START_MENU_TOKEN@*/.keys["3"]/*[[".keyboards.keys[\"3\"]",".keys[\"3\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key5.tap()
+        
+        let key6 = app/*@START_MENU_TOKEN@*/.keys["4"]/*[[".keyboards.keys[\"4\"]",".keys[\"4\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key6.tap()
+        
+        let key7 = app/*@START_MENU_TOKEN@*/.keys["5"]/*[[".keyboards.keys[\"5\"]",".keys[\"5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key7.tap()
+        
+        let key8 = app/*@START_MENU_TOKEN@*/.keys["6"]/*[[".keyboards.keys[\"6\"]",".keys[\"6\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key8.tap()
+        registerButton.tap()
+
     }
-    
-    
-
-    
-
         
-
         
-    //have to be logged in already
+        func testLogin() throws {
+            let app = XCUIApplication()
+            app.launch()
+            app.buttons["Login"].tap()
+            //app.buttons["Back"].tap()
+            //app.buttons["Register"].tap()
+            let testEmail = app.otherElements.textFields["Username"]
+            let testPass = app.otherElements.secureTextFields["Password"]
+            testEmail.tap()
+            testEmail.typeText("1@2.com")
+            testPass.tap()
+            testPass.typeText("123456")
+            app.buttons["Login"].tap()
+        }
+        
+        
+        //have to be logged in already
         func testExpiry() throws {
             let app = XCUIApplication()
             app.tabBars.buttons["Expiry"].tap()
             app.navigationBars["Reminders"].buttons["Add"].tap()
             app.searchFields["Enter Product"].tap()
-                   
-                //let app = app2
+            
+            //let app = app2
             app/*@START_MENU_TOKEN@*/.keys["A"]/*[[".keyboards.keys[\"A\"]",".keys[\"A\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-                   
+            
             let vKey = app/*@START_MENU_TOKEN@*/.keys["v"]/*[[".keyboards.keys[\"v\"]",".keys[\"v\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-                   vKey.tap()
-                   
-                   let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-                   oKey.tap()
-                   
-                   let cKey = app/*@START_MENU_TOKEN@*/.keys["c"]/*[[".keyboards.keys[\"c\"]",".keys[\"c\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-                   cKey.tap()
-                   
-                   let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-                   aKey.tap()
-                   
-                   let dKey = app/*@START_MENU_TOKEN@*/.keys["d"]/*[[".keyboards.keys[\"d\"]",".keys[\"d\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-                   dKey.tap()
+            vKey.tap()
+            
+            let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            oKey.tap()
+            
+            let cKey = app/*@START_MENU_TOKEN@*/.keys["c"]/*[[".keyboards.keys[\"c\"]",".keys[\"c\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            cKey.tap()
+            
+            let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            aKey.tap()
+            
+            let dKey = app/*@START_MENU_TOKEN@*/.keys["d"]/*[[".keyboards.keys[\"d\"]",".keys[\"d\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            dKey.tap()
             
             app/*@START_MENU_TOKEN@*/.tables.staticTexts["Avocado"]/*[[".otherElements[\"drop_down\"].tables",".cells.staticTexts[\"Avocado\"]",".staticTexts[\"Avocado\"]",".tables"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
             app.textFields["Enter Quantity"].tap()
@@ -88,7 +141,7 @@
             
         }
         
-
+        
         func testSaveBudget() throws{
             let app = XCUIApplication()
             
@@ -124,28 +177,12 @@
             let key3 = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
             key3.tap()
             app.navigationBars["New Budget"].buttons["Save"].tap()
-
+            
             
         }
         
         func testRecipe() throws{
-            let app = XCUIApplication()
-            app.launch()
-            app.tabBars["Tab Bar"].buttons["Recipe"].tap()
-            app.tables["Empty list"].children(matching: .searchField).element.tap()
-            app.typeText("Ranch")
-            app.buttons["Search"].tap()
-            app.tables/*@START_MENU_TOKEN@*/.staticTexts["Chicken Ranch Burgers"]/*[[".cells.staticTexts[\"Chicken Ranch Burgers\"]",".staticTexts[\"Chicken Ranch Burgers\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            
-            app.scrollViews.children(matching: .textView).element.swipeUp()
-            app/*@START_MENU_TOKEN@*/.textViews/*[[".scrollViews.textViews",".textViews"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.textViews["Step 4: Scoop and make 3 large burger patties."].tap()
-            
-            app.buttons["Recipes"].tap()
-            
-            
-            
-            
-            
+
         }
         
         func testNoteSave() throws
@@ -170,20 +207,20 @@
             
             let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
             eKey.tap()
-         
+            
             
             let rKey = app/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
             rKey.tap()
-        
+            
             
             let sKey = app/*@START_MENU_TOKEN@*/.keys["s"]/*[[".keyboards.keys[\"s\"]",".keys[\"s\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
             sKey.tap()
-         
+            
             app.children(matching: .window).element(boundBy: 0).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
             
             let moreKey = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
             moreKey.tap()
-           
+            
             
             app.keys["m"]
             app.tap()
@@ -197,12 +234,12 @@
             let spaceKey = app/*@START_MENU_TOKEN@*/.keys["space"]/*[[".keyboards.keys[\"space\"]",".keys[\"space\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
             spaceKey.tap()
             
-    
+            
             app.navigationBars["New Note"].buttons["Save"].tap()
             
             
         }
-
+        
         func testLaunchPerformance() throws {
             if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
                 // This measures how long it takes to launch your application.
@@ -212,8 +249,8 @@
             }
         }
     }
-
     
-
     
-
+    
+    
+    
