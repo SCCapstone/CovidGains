43f328327ca75326af781e380505dfead4c08745
Robert Carey <rtcarey@email.sc.edu>
2021-04-26 21:33:16 -0400

    added unit tests

--- CovidGainsTests/TestBudgetViewController.swift
+++ CovidGainsTests/TestBudgetViewController.swift
@@ -11,10 +11,35 @@ import Foundation
 
 class TestBudgetViewController: XCTestCase {
     
-    func testPressedAdd(){
-        
+    func testBudgetFB() {
         let budget = BudgetViewController()
-        budget.pressedAdd()
+        budget.db.collection("Test").getDocuments { (querySnapshot, error) in
+            if let e = error{
+                print("There is issue retrieving data.\(e)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let docID = doc.documentID
+                        XCTAssertNotEqual(docID, "")
+                    }
+                    
+                }
+            }
+        }
+    }
+    func testInsertDeleteBudgetData() {
+        let budget = BudgetViewController()
+        var newBudgetData = budget.budgetData
+        let newBudget = MyBudget(bProductName: "Example", bProductCost: "23")
+        newBudgetData.append(newBudget)
         
+        XCTAssertEqual(newBudget.bProductName, newBudgetData[newBudgetData.endIndex-1].bProductName)
+        
+        XCTAssertEqual(newBudget.bProductCost, newBudgetData[newBudgetData.endIndex-1].bProductCost)
+        
+        newBudgetData.remove(at: newBudgetData.endIndex-1)
+        let temp = newBudgetData.endIndex - 1
+        
+        XCTAssertFalse(newBudgetData.indices.contains(temp))
     }
 }
